fc86b546f8674a14f1610ca50fa58a4c3dc4be79
cshimegi <cs-himegi@hotmail.com>
2023-07-16 23:40:57 +0900

    feat: separate pc/sp app bars into components

---
+++ components/appBars/PcAppBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar color="teal-darken-4">
+    <v-app-bar-title>{{ title }}</v-app-bar-title>
+    <v-spacer></v-spacer>
+
+    <v-btn v-for="item in items" :key="item.title" :text="item.title" :to="item.to">
+      {{ item.title }}
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    title: String,
+    items: Array,
+  });
+</script>
